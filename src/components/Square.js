ed3235e677675223bc7ea36841345853473b69d8
Mateus Alexandre <mateus.alves@manabitechnology.com>
2023-07-14 17:01:23 +0000

    implementação dos componentes e funcionalidades

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,20 @@
 import React from "react";
 
-const Square = () => (
-<div>Square</div>);
+const Square = ({ value, onClick, color }) => {
+  const style = {
+    background: color,
+    border: "2px solid darkblue",
+    fontSize: "30px",
+    fontWeight: "800",
+    cursor: "pointer",
+    outline: "none",
+  };
+
+  return (
+    <button style={style} onClick={onClick}>
+      {value}
+    </button>
+  );
+};
 
 export default Square;
